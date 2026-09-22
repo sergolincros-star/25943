69cdd71d5db002e2781bfa5830f59f677a558fbd
Кузуб Сергей <s.kuzub@g.nsu.ru>
2026-09-22 15:49:12 +0700

    dodelal

--- s.kuzub/task2/ex_time.c
+++ s.kuzub/task2/ex_time.c
@@ -20,7 +20,7 @@ int main()
   printf("%d/%d/%02d %d:%02d %s\n",
          sp->tm_mon + 1,
          sp->tm_mday,
-         sp->tm_year,
+         sp->tm_year % 100,
          sp->tm_hour,
          sp->tm_min,
          tzname[sp->tm_isdst]);
